2d705ab0d10d5c30ed87ea5f2cc32eb313dcd6f4
tomtrasmontero <tomtrasmontero@gmail.com>
2017-06-10 21:21:44 -0400

    finished week 1 crud app

---
+++ crud_apps/acme_week1_assignment/product.model.js
@@ -0,0 +1,44 @@
+let _products = [
+	{
+		id:1,
+		name: "Shoe"
+	},
+	{
+		id:2,
+		name: "Shower Head"
+	}
+];
+
+module.exports = {
+	getProducts: () => _products,
+	add: (item) => _products.push({
+		id: _products.length,
+		name: item
+	}),
+	delete: function (id) {
+		let idx = 0;
+		
+		_products.map(function (e,i,arr) {
+			if(e.id === id){
+				idx = i;
+			}
+		}); 
+		_products.splice(idx,1);
+	},
+	getProduct: function(id){
+		let productItem = _products.filter(function (e,i,arr){
+			if(e.id === id) {
+				return e;
+			}
+		});
+		return productItem;
+	},
+	editProduct: function(name,id){
+		_products.forEach(function (e,i,arr){
+			if(e.id === id*1){
+				e.name = name;
+			}
+		});
+	}
+};
+
---
+++ crud_apps/acme_week1_assignment/products.routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const Product = require('./product.model');
+
+router.get('/', function(req,res,done) {
+	res.render('products', {title: 'products', products : Product.getProducts() })
+});
+
+router.get('/add', function(req,res,done) {
+	res.render('add', {title: 'add', products : Product})
+});
+
+router.post('/add/item', function(req,res,done){
+	Product.add(req.body.product);
+	res.redirect('/products');
+});
+
+router.delete('/delete/:id', function(req,res,done){
+	Product.delete(req.params.id);
+	res.redirect('/products');
+});
+
+
+router.get('/update/:productId/:productName', function(req, res, done){
+	res.render('edit', {title: 'edit', item: {id: req.params.productId, name: req.params.productName}});
+});	
+
+router.put('/edit/:itemId', function(req,res,done){
+	Product.editProduct(req.body.itemName, req.params.itemId);
+	res.redirect('/products');
+});
+
+module.exports = router;
