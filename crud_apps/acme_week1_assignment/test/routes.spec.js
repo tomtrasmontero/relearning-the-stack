12ed5cff1c9e3608c2c525e201aec55dcb724510
tomtrasmontero <tomtrasmontero@gmail.com>
2017-06-12 00:21:56 -0400

    added new test spec

--- crud_apps/acme_week1_assignment/test/routes.spec.js
+++ crud_apps/acme_week1_assignment/test/routes.spec.js
@@ -1,3 +1,32 @@
 const expect = require('chai').expect;
 const Product = require('../product.model');
 const app = require('supertest')(require('../app'));
+
+describe('routes', function(){
+	beforeEach(function(){
+		Product.resetSeed();
+	});
+
+	describe('GET /', function(){
+		it('has a products title', function(done){
+			app.get('/')
+			.expect(200)
+			.end(function(err,res){
+				expect(res.text).to.contain('products');
+				expect(200,'ok');
+				done();
+			})
+		})
+	});
+
+	describe('GET /add', function(){
+		it('takes you to a new page', function(done){
+			app.get('/add')
+			.expect(200)
+			.end(function(err, res){
+				expect(res.text).to.contain('add');
+				done();
+			})
+		})
+	});
+})
