79cec24100bde9d064ba461f3f91ff7bec10123d
tomtrasmontero <tomtrasmontero@gmail.com>
2017-06-12 00:22:33 -0400

    added new test spec

--- crud_apps/acme_week1_prof_sol/server.js
+++ crud_apps/acme_week1_prof_sol/server.js
@@ -3,5 +3,5 @@ const http = require('http');
 const server = http.createServer(require('./app'));
 
 server.listen(process.env.PORT, function(){
-	console.log(`listening on ${process.env.PORT}`);
+	console.log(`listening on port ${process.env.PORT}`);
 });
