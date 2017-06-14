9ae58ff77a5a25c2db5fe224387bae01bb87530d
tomtrasmontero <tomtrasmontero@gmail.com>
2017-06-13 22:57:40 -0400

    updated models and updated test spec

--- crud_apps/acme_categories_orm_ex/db.js
+++ crud_apps/acme_categories_orm_ex/db.js
@@ -1,7 +1,53 @@
-const Category = {};
+const Sequelize = require('sequelize');
+
+//access into database access based on package.json
+const db = new Sequelize(process.env.CONN);
+
+//create database tables
+//name of category
+const Category = db.define('category', {
+	name: Sequelize.STRING
+});
+
+//new way to have a class method in Category.
+Category.findByName = function(name){
+	return this.findAll({
+		where: {name: name}
+	});
+};
+
+const Product = db.define('product', {
+	name: Sequelize.STRING
+});
+
+let _conn;
+
+function connect(){
+	if( _conn){
+		return _conn;
+	} else {
+		//db.authenticate() returns a promise
+		_conn = db.authenticate();
+		return _conn;
+	}
+}
+
+function seed() {
+//connect to database
+	return connect()
+	.then( () => db.sync({ force: true }))
+	.then( () => {
+		let fooCreate = Category.create({ name: 'foo'} );
+		let barCreate = Category.create({ name: 'bar'} );
+		return Promise.all([ fooCreate, barCreate]);
+	}) 
+
+}
 
 module.exports = {
 	models: {
-		Category: Category
-	}
+		Category: Category,
+		Product: Product
+	},
+	seed: seed
 };
--- crud_apps/acme_categories_orm_ex/test/models/category.model.spec.js
+++ crud_apps/acme_categories_orm_ex/test/models/category.model.spec.js
@@ -1,14 +1,53 @@
 const expect = require('chai').expect;
 const db = require('../../db.js');
 const Category = db.models.Category;
+const Product = db.models.Product;
 
-describe('Category', function(){
+
+describe('Category & Product', function(){
 	beforeEach(function(done){
+		let foo, bar;
 		db.seed()
-			.then(() => done())
-			.catch( (err) => done(err))
+		.then((result) => {
+			//.get() will return the item that is in the database;
+			// console.log(result[0].get());
+			foo = result[0];
+			bar = result[1];
+			done()
+		})
+		.catch( (err) => done(err))
 	
 	});
 
-	it('exists', () => expect(Category).to.be.ok);
+	it('exists', () => {
+		expect(Category).to.be.ok;
+		expect(Product).to.be.ok;
+	});
+
+	describe('#findByName', function(){
+		it('there is one category named foo', function(done){
+			
+			console.log('++++++++++',Category);
+			Category.findByName('foo')
+			.then( (categories) => {
+				expect(categories.length).to.equal(1);
+				expect(categories[0].name).to.equal('foo');
+				done();
+			})
+			.catch( (err) => done(err))
+		});
+	});
+
+
+	describe('#findAll', function(){
+		it('there are two categories', function(done){
+			Category.findAll()
+			.then( (categories) => {
+				expect(categories.length).to.equal(2);
+				done()
+			})
+			.catch( (err) => done(err))	
+		});
+	});
+
 });
