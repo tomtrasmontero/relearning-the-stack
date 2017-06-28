51596c6eebedba8f6f58ff56f27bc81b19ac2d9f
tomtrasmontero <tomtrasmontero@gmail.com>
2017-06-28 02:22:28 -0400

    added rootScope ng-if examples

--- angular-intro/app.js
+++ angular-intro/app.js
@@ -2,7 +2,8 @@
 var app = angular.module('app', []);
 
 //creating a controller (name of controller, function )
-app.controller('TodoCtrl', ($http,$scope) => {
+//pubsub style using rootScope
+app.controller('TodoCtrl', ($http,$scope, $rootScope) => {
 	$scope.todos = [
 		{id: 1, name: 'foo'},
 		{id: 2, name: 'bar'}
@@ -10,6 +11,29 @@ app.controller('TodoCtrl', ($http,$scope) => {
 
 	$scope.destroy = (itemId) => {
 		$scope.todos = $scope.todos.filter((ele,idx,arr) => itemId !== ele.id);
-		console.log('todos deleted!');
+		$rootScope.$broadcast('message', 'destroyed Todo');
 	};
+
+	$scope.create = () => {
+		let maxId = $scope.todos.reduce( (memo, todo) => {
+			if(todo.id > memo)
+				memo = todo.id;
+			return memo;
+		}, 0);
+			
+
+		$scope.todos.push({ id: ++maxId , name: $scope.name });
+
+		$rootScope.$broadcast('message', 'created Todo');
+		
+		//clear input field
+		$scope.name='';
+	};
+});
+
+app.controller('MessageCtrl', ($scope,$timeout) => {
+	$scope.$on('message', (ev, message) => {
+		console.log(ev);
+		$scope.message = message;
+	});
 });
