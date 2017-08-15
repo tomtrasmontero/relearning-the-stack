4e89979fb2a93da0b400580e57d36ff4d80d3db6
tomtrasmontero <tomtrasmontero@gmail.com>
2017-08-15 02:26:39 -0400

    update files

--- react_intro/src/UserDetail.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UserDetail = ({ user , onDeselect }) => (
-	<div className='well' onClick={ () => onDeselect() }>
-		{ user.name }
-	</div>	
-);
-
-export default UserDetail;
